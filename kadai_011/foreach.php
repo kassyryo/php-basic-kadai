98ece379c588c4a64ab27fdf2823e3d32ce86f0b
kassyryo <32595570+kassyryo@users.noreply.github.com>
2024-06-08 18:29:35 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        $shohin=['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道'];
+
+        foreach($shohin as $key=>$value){
+            echo "{$key}:{$value}<br>";
+        }
+        
+        ?>
+    </p>
+</body>
+</html>
